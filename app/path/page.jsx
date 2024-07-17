5929d6518287ff3f5f9a2ba652794e919461ce93
Prince Jain <pj122333444@gmail.com>
2024-07-17 16:36:38 +0530

    changes

---
+++ app/path/page.jsx
@@ -0,0 +1,17 @@
+// home - http://localhost:3000
+// location all- http://localhost:3000/location/all
+// location create- http://localhost:3000/location/create
+// location edit - http://localhost:3000/location/edit/3
+// location view- http://localhost:3000/location/view/3
+
+// announcemnt all - http://localhost:3000/announcement/all
+// announcemnt new - http://localhost:3000/announcement/new
+// announcemnt edit -http://localhost:3000/announcement/edit/d
+// announcemnt view - http://localhost:3000/announcement/view/e
+
+// announcemnt validate 1 - http://localhost:3000/announcement/validate/1/d
+// announcemnt validate 2 - http://localhost:3000/announcement/validate/2/d
+// announcemnt validate 3 - http://localhost:3000/announcement/validate/3/d
+
+// login -http://localhost:3000/login
+// contact-http://localhost:3000/contact
